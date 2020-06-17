03d36127f72e54efabbee938fd54efcd72e06a48
GiftedGeek <rajeshkumar19nintyeight@gmail.com>
2020-06-18 02:56:35 +0530

    Modified JSON and updated card info

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from "react";
 
 const Card = (props) => {
-    const {name, email, id} = props
+    const {name, username, email, id} = props
   return (
     <Fragment>
       <div className="tc bg-green near-white dib br3 pa2 ma2 grow bw1 shadow-4">
+          <h2>{name}</h2>
         <img src={`https://robohash.org/${id}?size=200x200`} alt="Robot" />
         <div>
-          <h2>{name}</h2>
+          <p>{username}</p>
           <p>{email}</p>
+
         </div>
       </div>
     </Fragment>
