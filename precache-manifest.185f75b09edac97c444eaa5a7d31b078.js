5b19e9cdda0b0afb50d5a0c409acab3db391e082
GiftedGeek <rajeshkumar19nintyeight@gmail.com>
2020-06-22 02:46:28 +0530

    Updates

---
+++ precache-manifest.185f75b09edac97c444eaa5a7d31b078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbd9edba7f87ff4ba25ba4596a16f58",
+    "url": "/robosocial/index.html"
+  },
+  {
+    "revision": "bcd9e13e17971abdf174",
+    "url": "/robosocial/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f8f518249fe337b2725a",
+    "url": "/robosocial/static/css/main.130e3e3b.chunk.css"
+  },
+  {
+    "revision": "bcd9e13e17971abdf174",
+    "url": "/robosocial/static/js/2.f09406fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robosocial/static/js/2.f09406fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f518249fe337b2725a",
+    "url": "/robosocial/static/js/main.87988712.chunk.js"
+  },
+  {
+    "revision": "c05dea229d66cf840d32",
+    "url": "/robosocial/static/js/runtime-main.cf953cf6.js"
+  }
+]);
